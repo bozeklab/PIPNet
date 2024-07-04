1edf75736c9297586b9c6e33848bc1b7ac7b2735
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-07-05 00:38:07 +0200

    resnet50

--- pipnet/pipnet.py
+++ pipnet/pipnet.py
@@ -2,7 +2,7 @@ import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from features.resnet_features import resnet18_features, resnet34_features, resnet50_features, resnet50_features_inat, resnet101_features, resnet152_features
+from features.resnet_features import resnet18_features, resnet34_features, resnet50_features, resnet101_features, resnet152_features
 from features.convnext_features import convnext_tiny_26_features, convnext_tiny_13_features 
 import torch
 from torch import Tensor
@@ -44,7 +44,7 @@ class PIPNet(nn.Module):
 base_architecture_to_features = {'resnet18': resnet18_features,
                                  'resnet34': resnet34_features,
                                  'resnet50': resnet50_features,
-                                 'resnet50_inat': resnet50_features_inat,
+                                 #'resnet50_inat': resnet50_features_inat,
                                  'resnet101': resnet101_features,
                                  'resnet152': resnet152_features,
                                  'convnext_tiny_26': convnext_tiny_26_features,
