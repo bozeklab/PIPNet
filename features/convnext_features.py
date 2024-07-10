a79ec336df5113d5a73f9c5aeeee7a2a0c99b5e1
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-07-10 10:53:29 +0200

    patch size corrected

--- features/convnext_features.py
+++ features/convnext_features.py
@@ -9,6 +9,8 @@ def replace_convlayers_convnext(model, threshold):
         if isinstance(module, nn.Conv2d):
             if module.stride[0] == 2:
                 if module.in_channels > threshold: #replace bigger strides to reduce receptive field, skip some 2x2 layers. >100 gives output size (26, 26). >300 gives (13, 13)
+                    print('!!!!')
+                    print(module.stride)
                     module.stride = tuple(s//2 for s in module.stride)
                     
     return model
