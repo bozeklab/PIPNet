65ae5548d9eab3f2ebd0ddcc09678c2c180d6e6a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-07-12 11:11:36 +0200

    patch size corrected

--- pipnet/train.py
+++ pipnet/train.py
@@ -106,7 +106,7 @@ def calculate_loss(proto_features, pooled, hflip, out, ys1, align_pf_weight, t_w
     N = hflip.shape[0]
     for i in range(N):
         if hflip[i]:
-            pf1[i] = torch.flip(pf1[i], [3])
+            pf1[i] = torch.flip(pf1[i], [2])
 
     embv2 = pf2.flatten(start_dim=2).permute(0,2,1).flatten(end_dim=1)
     embv1 = pf1.flatten(start_dim=2).permute(0,2,1).flatten(end_dim=1)
