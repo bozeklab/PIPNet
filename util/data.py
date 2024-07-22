60989fba0e5a7348aa7ca93f6ebf26e514e51ecc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-07-22 12:05:11 +0200

    masks

--- util/data.py
+++ util/data.py
@@ -447,6 +447,7 @@ class FourAugSupervisedDataset(torch.utils.data.Dataset):
             self.targets = dataset._labels
             self.imgs = list(zip(dataset._image_files, dataset._labels))
         self.imgs = [img for img in self.imgs if "mask" not in img]
+        print(self.imgs)
         self.transform1 = transform1
         self.transform2 = transform2
         self.transform3 = transform3
