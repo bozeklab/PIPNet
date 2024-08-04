dd4d7f0397cdea8ef898eff80107504fdad14529
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-04 11:18:03 +0200

    debug

--- util/vis_pipnet.py
+++ util/vis_pipnet.py
@@ -326,9 +326,11 @@ def visualize(net, projectloader, num_classes, device, foldername, args: argpars
                     save_path = os.path.join(dir, "prototype_%s")%str(p)
                     if not os.path.exists(save_path):
                         os.makedirs(save_path)
-                    image = Image.alpha_composite(image, mask)
+                    print(msk_tensor)
+                    print(img_tensor)
+                    output = Image.fromarray((input_image * (0.6 * output / 255 + 0.4)).astype(np.uint8))
                     draw = D.Draw(image)
-                    draw.rectangle([(w_coor_min,h_coor_min), (w_coor_max, h_coor_max)], outline=boundary_color, width=2)
+                    draw.rectangle([(w_coor_min, h_coor_min), (w_coor_max, h_coor_max)], outline=boundary_color, width=2)
                     image.save(os.path.join(save_path, 'p%s_%s_%s_%s_rect.png'%(str(p),str(imglabel),str(round(found_max, 2)),str(img_to_open.split('/')[-1].split('.jpg')[0]))))
 
         images_seen_before+=len(ys)
