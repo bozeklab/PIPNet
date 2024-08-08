b0d0be652b3e1f40e98391dbecfd26c64982326c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-08 19:53:14 +0200

    debug

--- util/vis_pipnet.py
+++ util/vis_pipnet.py
@@ -311,7 +311,7 @@ def visualize(net, projectloader, num_classes, device, foldername, args: argpars
                     img_tensor = transforms.ToTensor()(image).unsqueeze_(0) #shape (1, 3, h, w)
                     h_coor_min, h_coor_max, w_coor_min, w_coor_max = get_img_coordinates(img_size, softmaxes.shape, patchsize, skip, h_idx, w_idx)
                     img_tensor_patch = img_tensor[0, :, h_coor_min:h_coor_max, w_coor_min:w_coor_max]
-                    msk_tensor_patch = msk_tensor[:, h_coor_min:h_coor_max, w_coor_min:w_coor_max]
+                    msk_tensor_patch = bool_mask[h_coor_min:h_coor_max, w_coor_min:w_coor_max]
                     saved[p]+=1
                     tensors_per_prototype[p].append((img_tensor_patch, found_max))
 
