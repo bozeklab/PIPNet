b50295eede163eada099493957620b01a0eabc6f
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-23 14:43:24 +0200

    refactor

--- util/grayscale_test_ds.py
+++ util/grayscale_test_ds.py
@@ -5,9 +5,11 @@ import os
 import torch.optim
 import torch.utils.data
 import torchvision
+from PIL import Image, ImageDraw as D
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 from typing import Tuple, Dict
+import matplotlib.patches as patches
 from torch import Tensor
 import random
 from sklearn.model_selection import train_test_split
@@ -73,7 +75,7 @@ def get_grayscale(augment:bool, train_dir: str, project_dir: str, test_dir:str, 
                            train_dir, project_dir, test_dir, seed, validation_size)
 
 
-def display_tensor_images(xs, xs_ds, m, m_ds):
+def display_tensor_images(xs, xs_ds, m, m_ds, h_coor_min, h_coor_max, w_coor_min, w_coor_max):
     # Ensure the tensors are on the CPU and convert them to NumPy arrays
     xs = xs.cpu().numpy()
     xs_ds = xs_ds.cpu().numpy()
@@ -86,8 +88,6 @@ def display_tensor_images(xs, xs_ds, m, m_ds):
     # Transpose the arrays from [C, H, W] to [H, W, C]
     image = np.transpose(xs, (1, 2, 0))
     image_ds = np.transpose(xs_ds, (1, 2, 0))
-    m = np.transpose(m, (1, 2, 0))
-    m_ds = np.transpose(m_ds, (1, 2, 0))
 
     # Denormalize if necessary (assuming the images were normalized to [-1, 1])
     image = (image - image.min()) / (image.max() - image.min())
@@ -108,6 +108,10 @@ def display_tensor_images(xs, xs_ds, m, m_ds):
     axes[0].axis('off')  # Turn off axis labels
     axes[0].set_title('xs1')
 
+    rect = patches.Rectangle((w_coor_min, h_coor_min), w_coor_max - w_coor_min, h_coor_max - h_coor_min,
+                             linewidth=1, edgecolor='r', facecolor='none')
+    axes[0].add_patch(rect)
+
     axes[1].imshow(image_ds, cmap='gray')
     axes[1].axis('off')  # Turn off axis labels
     axes[1].set_title('xs2')
@@ -115,6 +119,9 @@ def display_tensor_images(xs, xs_ds, m, m_ds):
     axes[2].imshow(m, cmap='gray')
     axes[2].axis('off')  # Turn off axis labels
     axes[2].set_title('mask')
+    rect = patches.Rectangle((w_coor_min, h_coor_min), w_coor_max - w_coor_min, h_coor_max - h_coor_min,
+                             linewidth=1, edgecolor='r', facecolor='none')
+    axes[2].add_patch(rect)
 
     axes[3].imshow(m_ds, cmap='gray')
     axes[3].axis('off')  # Turn off axis labels
@@ -124,7 +131,21 @@ def display_tensor_images(xs, xs_ds, m, m_ds):
 
 
 def main():
-    root_dir = '/Users/piotrwojcik/Downloads/mito_work/dataset'
+    root_dir = '/Users/piotrwojcik/Downloads/mito_work/dataset_512'
+
+    example_img = '/Users/piotrwojcik/Downloads/mito_work/dataset_512/train/0_fl_fl/10kX_919_wt__0067.png'
+    example_mask = '/Users/piotrwojcik/Downloads/mito_work/dataset_512/train/0_fl_fl/mask_10kX_919_wt__0067.png'
+
+    image = transforms.Resize(size=(448, 448))(Image.open(example_img).convert("RGB"))
+    mask = transforms.Resize(size=(448, 448))(Image.open(example_mask).convert("RGB"))
+    msk_tensor = transforms.ToTensor()(mask)
+    img_tensor = (transforms.ToTensor()(image) * 255).int()  # shape (1, 3, h, w)
+
+    output = (img_tensor.numpy() * (0.6 * msk_tensor.numpy() + 0.4)).astype(np.uint8)
+    output_image = Image.fromarray(np.squeeze(output).transpose(1, 2, 0))
+    output_image.show()
+
+
     image_size = 448
     image_size_ds = 224
     seed = 1
@@ -137,11 +158,18 @@ def main():
     print(len(projectset))
     for idx in range(len(testset)):
         xs, xs_ds, m, m_ds, ys = testset[idx]
-        print(idx)
-        print(xs.shape)
-        print(xs_ds.shape)
 
-        display_tensor_images(xs, xs_ds, m, m_ds)
+        h_coor_min = 141
+        w_coor_min = 130
+        delta = 32
+        msk_tensor_patch = m[h_coor_min:(h_coor_min + delta), w_coor_min:(w_coor_min + delta)]
+        num_white_pixels = torch.sum(msk_tensor_patch).item()
+        print(num_white_pixels)
+        print(msk_tensor_patch)
+        print(msk_tensor_patch.shape)
+
+        #display_tensor_images(xs, xs_ds, m, m_ds, h_coor_min, (h_coor_min + delta),
+        #                      w_coor_min, (w_coor_min + delta))
         #if idx >= 15:
         #    break
 
