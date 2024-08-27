6935e5d3ceeafa76e85b64c9132692792453ea98
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-27 13:10:23 +0200

    no flip

--- util/data.py
+++ util/data.py
@@ -557,7 +557,7 @@ class FourAugSupervisedDataset(torch.utils.data.Dataset):
 
 class RandomHorizontalFlip(transforms.RandomHorizontalFlip):
     def forward(self, img):
-        if torch.rand(1) < 0.0:
+        if torch.rand(1) < 0.3:
             return F.hflip(img), True
         return img, False
 
