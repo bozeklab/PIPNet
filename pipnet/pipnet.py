b7b5b9e041e1c5d306feccdca5d87dcc89124b44
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-07 02:12:23 +0200

    important last min

--- pipnet/pipnet.py
+++ pipnet/pipnet.py
@@ -35,7 +35,7 @@ class PIPNet(nn.Module):
         proto_features_ds = self._add_on(features_ds)
         B, D, H, W = proto_features.shape
         _, _, H_ds, W_ds = proto_features_ds.shape
-        proto_features_ds_ups = proto_features_ds.repeat_interleave(2, dim=1).repeat_interleave(2, dim=2)
+        proto_features_ds_ups = proto_features_ds.repeat_interleave(2, dim=2).repeat_interleave(2, dim=3)
         p_f = proto_features.view(B, D, -1).permute(0, 2, 1) #B, HW, D/2
         p_f_ds = proto_features_ds_ups.view(B, D, -1).permute(0, 2, 1) #B, H'W', D/2
         #p_f_ds_ups = p_f_ds.repeat_interleave(2, dim=1).repeat_interleave(2, dim=2) #B, HW, D/2
