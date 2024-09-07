8ae8d950dff9b85de5ca691f372e2e5a93a3d28c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-07 02:20:54 +0200

    important last min

--- pipnet/pipnet.py
+++ pipnet/pipnet.py
@@ -37,9 +37,9 @@ class PIPNet(nn.Module):
         _, _, H_ds, W_ds = proto_features_ds.shape
         proto_features_ds_ups = proto_features_ds.repeat_interleave(2, dim=2).repeat_interleave(2, dim=3)
         p_f = proto_features.view(B, D, -1).permute(0, 2, 1) #B, HW, D/2
-        p_f_ds = proto_features_ds_ups.view(B, D, -1).permute(0, 2, 1) #B, H'W', D/2
+        p_f_ds = proto_features_ds_ups.view(B, D, -1).permute(0, 2, 1) #B, HW, D/2
         #p_f_ds_ups = p_f_ds.repeat_interleave(2, dim=1).repeat_interleave(2, dim=2) #B, HW, D/2
-        combined = torch.cat([p_f, p_f_ds], dim=0) #B, HW, D
+        combined = torch.cat([p_f, p_f_ds], dim=2) #B, HW, D
         combined = combined.view(B, H, W, D*2).permute(0, 3, 1, 2)
         #softmax_combined = F.softmax(combined, dim=1)
 
