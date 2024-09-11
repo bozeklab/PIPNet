b2be9945226d8b8775257bcc6984c19464aba588
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-11 19:23:12 +0200

    important last min

--- util/vis_pipnet.py
+++ util/vis_pipnet.py
@@ -141,7 +141,7 @@ def visualize_topk(net, projectloader, num_classes, device, foldername, args: ar
 
                                 draw = D.Draw(pil_image)
                                 draw.text((img_tensor_patch.shape[0] // 2, img_tensor_patch.shape[1] // 2), str(max_per_prototype),
-                                          font=font,
+                                          #font=font,
                                           anchor='mm', fill="red")
                                 txttensor = transforms.ToTensor()(pil_image)
 
