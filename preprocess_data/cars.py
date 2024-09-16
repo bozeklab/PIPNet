7588403494b6e4c631438b815e61507b1c9ebef3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-16 20:21:48 +0200

    add cars

--- preprocess_data/cars.py
+++ preprocess_data/cars.py
@@ -13,7 +13,7 @@ if not os.path.isdir(path):
 time_start = time.time()
 
 # convert ids to class names
-class_names = scipy.io.loadmat('./data/cars/devkit/cars_meta.mat')
+class_names = scipy.io.loadmat('./data/cars/devkit/cars_annos.mat')
 class_ids_to_names = dict()
 for row in range(len(class_names['class_names'][0])):
     name = class_names['class_names'][0][row][0]
@@ -22,7 +22,7 @@ for row in range(len(class_names['class_names'][0])):
     class_ids_to_names[row + 1] = name
 
 # adapted from https://github.com/tonylaioffer/cnn_car_classification/blob/master/data_prepare.py
-mat = scipy.io.loadmat('./data/cars/devkit/cars_train_annos.mat')
+mat = scipy.io.loadmat('./data/cars/devkit/cars_annos.mat')
 # print("annotations: ", mat['annotations'])
 training_class = mat['annotations']['class']
 training_fname = mat['annotations']['fname']
@@ -31,36 +31,39 @@ training_y1 = mat['annotations']['bbox_y1']
 training_x2 = mat['annotations']['bbox_x2']
 training_y2 = mat['annotations']['bbox_y2']
 
-mat = scipy.io.loadmat('./data/cars/devkit/cars_test_annos_withlabels.mat')
+mat = scipy.io.loadmat('/data/pwojcik/stan_cars/cars_annos.mat')
 # print(mat['annotations'])
 testing_class = mat['annotations']['class']
 testing_fname = mat['annotations']['fname']
 
-training_source = './data/cars/cars_train/'  # specify source training image path
+training_source = '/data/pwojcik/stan_cars/cars_train/'  # specify source training image path
 training_output = path + 'train/'  # specify target trainig image path (trainig images need to be orgnized to specific structure)
-if not os.path.exists(training_output):
-    os.mkdir(training_output)
+
+testing_source = '/data/pwojcik/stan_cars/cars_test/'  # specify source testing image path
+testing_output = path + 'test/'  # specify target testing image path (testing images need to be orgnized to specific structure)
+
+
+for directory in [training_output, testing_output]:
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
 for idx, cls in enumerate(training_class[0]):
     cls = cls[0][0]
     fname = training_fname[0][idx][0]
+    fname = os.path.basename(fname)
+
+    train_path = os.path.join(training_source, fname)
+    test_path = os.path.join(testing_source, fname)
     print(cls, class_ids_to_names[cls], fname)
-    output_path = os.path.join(training_output, class_ids_to_names[cls])
+
+    if os.path.exists(train_path):
+        output_path = os.path.join(training_output, class_ids_to_names[cls])
+    else:
+        output_path = os.path.join(testing_output, class_ids_to_names[cls])
     if not os.path.exists(output_path):
         os.mkdir(output_path)
     shutil.copy(os.path.join(training_source, fname), os.path.join(output_path, fname))
 
-testing_source = './data/cars/cars_test/'  # specify source testing image path
-testing_output = path + 'test/'  # specify target testing image path (testing images need to be orgnized to specific structure)
-if not os.path.exists(testing_output):
-    os.mkdir(testing_output)
-for idx, cls in enumerate(testing_class[0]):
-    cls = cls[0][0]
-    fname = testing_fname[0][idx][0]
-    output_path = os.path.join(testing_output, class_ids_to_names[cls])
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-    shutil.copy(os.path.join(testing_source, fname), os.path.join(output_path, fname))
 
 time_end = time.time()
 print('Cars dataset processed, %s!' % (time_end - time_start))
