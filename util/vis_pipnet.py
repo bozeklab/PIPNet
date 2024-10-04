86959b08fa0134269f4de32a095eb6b5f39648b0
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-04 17:18:02 +0200

    without masks

--- util/vis_pipnet.py
+++ util/vis_pipnet.py
@@ -101,7 +101,9 @@ def visualize_topk(net, projectloader, num_classes, device, foldername, args: ar
                     mininterval=50.,
                     desc='Visualizing topk',
                     ncols=0)
-    for i, (xs, ys) in img_iter: #shuffle is false so should lead to same order as in imgs
+    for i, sample in img_iter: #shuffle is false so should lead to same order as in imgs
+        xs, ys = sample['image']
+        filename = sample['filename']
         if i in alli:
             xs, ys = xs.to(device), ys.to(device)
             for p in topks.keys():
