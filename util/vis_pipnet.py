cf61473be98eab081b9559c7082cec8730130d1e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-12 23:39:19 +0100

    confusion

--- util/vis_pipnet.py
+++ util/vis_pipnet.py
@@ -133,7 +133,7 @@ def visualize_topk(net, projectloader, num_classes, device, foldername, args: ar
     selected_dists = random.sample(list(img_dist.items()), 10)
     for dist_id, dist in enumerate(selected_dists):
         y = dist[1]
-        print(dist[0])
+        print('!!! ', len(dist[0]))
         grid_image = build_image_grid(dist[0])
         #save_path = os.path.join(dir,  f"{dist_id}_grid_{y}.png")
         #grid_image.save(save_path)
