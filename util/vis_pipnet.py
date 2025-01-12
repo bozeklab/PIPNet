85337f3254f4b9da1b10fbffbd548723cde095cf
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-13 00:36:46 +0100

    confusion

--- util/vis_pipnet.py
+++ util/vis_pipnet.py
@@ -139,7 +139,8 @@ def visualize_topk(net, projectloader, num_classes, device, foldername, args: ar
         dist_ps = []
         for d in dist[1]:
             dist_ps.append(visualize_dist(d[0]))
-        grid_image = build_image_grid(dist_ps)
+        sorted_dis, _ = torch.sort(dist_ps)
+        grid_image = build_image_grid(sorted_dis)
         save_path = os.path.join(dir,  f"{dist_id}_grid_{y}.png")
         grid_image.save(save_path)
 
