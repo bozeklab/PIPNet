235de41fe09f75217446f11dd0d429d7d3d70eb8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-17 02:41:28 +0100

    added CKA loss

--- pipnet/train.py
+++ pipnet/train.py
@@ -119,9 +119,9 @@ def calculate_loss(proto_features, pooled, out, ys1, align_pf_weight, t_weight, 
 
     if not finetune:
         loss = align_pf_weight*a_loss_pf
-        loss += t_weight * tanh_loss
+        loss += 5 * t_weight * tanh_loss
         ck_loss = cka.forward(feature_map=pf1) + cka.forward(feature_map=pf2)
-        #loss += 0.1 * ck_loss
+        loss += 0.1 * ck_loss
         #print(ck_loss, tanh_loss, t_weight * tanh_loss)
     
     if not pretrain:
