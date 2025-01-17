f5963ea10c3fd8768b85e326ecdfc0b1b2a410c9
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-17 01:53:09 +0100

    added CKA loss

--- pipnet/train.py
+++ pipnet/train.py
@@ -121,7 +121,8 @@ def calculate_loss(proto_features, pooled, out, ys1, align_pf_weight, t_weight, 
     if not finetune:
         loss = align_pf_weight*a_loss_pf
         loss += t_weight * tanh_loss
-        cka.forward(pf1)
+        ck_loss = cka.forward(feature_map=pf1)
+        print(ck_loss, tanh_loss, t_weight * tanh_loss)
     
     if not pretrain:
         softmax_inputs = torch.log1p(out**net_normalization_multiplier)
