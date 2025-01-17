695de29a1be29b9cc2ffdbae9540d873697d8d4c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-17 01:27:03 +0100

    added CKA loss

--- pipnet/train.py
+++ pipnet/train.py
@@ -66,6 +66,7 @@ def train_pipnet(net, train_loader, optimizer_net, optimizer_classifier, schedul
        
         # Perform a forward pass through the network
         proto_features, pooled, out = net(torch.cat([xs1, xs2]))
+        print('!!! ', proto_features.shape)
         loss, acc = calculate_loss(proto_features, pooled, out, ys, align_pf_weight, t_weight, unif_weight, cl_weight, net.module._classification.normalization_multiplier, pretrain, finetune, criterion, train_iter, print=True, EPS=1e-8)
         
         # Compute the gradient
@@ -176,7 +177,7 @@ def calculate_loss(proto_features, pooled, out, ys1, align_pf_weight, t_weight, 
     
     a_loss_pf = (align_loss(embv1, embv2.detach())+ align_loss(embv2, embv1.detach()))/2.
     tanh_loss = -(torch.log(torch.tanh(torch.sum(pooled1,dim=0))+EPS).mean() + torch.log(torch.tanh(torch.sum(pooled2,dim=0))+EPS).mean())/2.
-    print('!!! ', proto_features.shape)
+
     ##CKA = CKA_loss()
 
 
