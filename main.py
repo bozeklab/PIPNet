7ea089d69bf7f7b154485dc8717b48c152d27993
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-28 20:38:21 +0100

    mnist

--- main.py
+++ main.py
@@ -144,6 +144,8 @@ def run_pipnet(args=None):
                     class_0_features.append(proto_features[i].flatten(1))
                     print(id, ' ', class_0_features[-1].shape)
             args.wshape = wshape  # needed for calculating image patch size
+            if id > 500:
+                break
             #print("Output shape: ", proto_features.shape, ' ys = ', ys, flush=True)
 
     print('Concat')
