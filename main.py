c0911ab097fc92d2b4a7c3a5d3c7935c6f41f2b5
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-28 18:52:59 +0100

    mnist

--- main.py
+++ main.py
@@ -131,6 +131,8 @@ def run_pipnet(args=None):
         args.wshape = wshape #needed for calculating image patch size
         print("Output shape: ", proto_features.shape, flush=True)
 
+
+    print(len(trainloader))
     with torch.no_grad():
         for xs1, _, ys in trainloader:
             xs1 = xs1.to(device)
