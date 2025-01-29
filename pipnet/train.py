23971e50e521e81cd7b19b45d2120f6e2a07fbcf
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-29 12:13:36 +0100

    mnist

--- pipnet/train.py
+++ pipnet/train.py
@@ -116,11 +116,11 @@ def calculate_loss(proto_features, pooled, out, ys1, align_pf_weight, t_weight, 
     tanh_loss = -(torch.log(torch.tanh(torch.sum(pooled1,dim=0))+EPS).mean() + torch.log(torch.tanh(torch.sum(pooled2,dim=0))+EPS).mean())/2.
 
     cka = CKA_loss(concept_cha=1)
+    ck_loss = cka.forward(feature_map=pf1) + cka.forward(feature_map=pf2)
 
     if not finetune:
         loss = align_pf_weight*a_loss_pf
         #loss += t_weight * t_weight * tanh_loss
-        ck_loss = cka.forward(feature_map=pf1) + cka.forward(feature_map=pf2)
         loss += 0.1 * ck_loss
         print(ck_loss, tanh_loss, t_weight * tanh_loss)
     
