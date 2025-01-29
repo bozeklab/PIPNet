f1ca9acba0d681bdbf63666639be62c7781343d9
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-29 21:04:28 +0100

    mnist

--- pipnet/train.py
+++ pipnet/train.py
@@ -121,8 +121,8 @@ def calculate_loss(proto_features, pooled, out, ys1, align_pf_weight, t_weight, 
 
     if not finetune:
         loss = align_pf_weight*a_loss_pf
-        #loss += t_weight * t_weight * tanh_loss
-        loss += ck_loss
+        loss += t_weight * t_weight * tanh_loss
+        #loss += ck_loss
         #print(ck_loss, tanh_loss, t_weight * tanh_loss)
     
     if not pretrain:
