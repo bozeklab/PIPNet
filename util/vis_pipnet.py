94ad23b56fd64552a466f54974bce8097607fc9a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-30 15:00:33 +0100

    mnist

--- util/vis_pipnet.py
+++ util/vis_pipnet.py
@@ -138,7 +138,7 @@ def visualize_topk(net, projectloader, num_classes, device, foldername, args: ar
         #print('!!! ', len(dist[1][0]))
         dist_ps = []
         for d in dist[1]:
-            dist_ps.append(visualize_dist(torch.sort(d[0] * d[0])[0]))
+            dist_ps.append(visualize_dist(d[0]))
         grid_image = build_image_grid(dist_ps)
         save_path = os.path.join(dir,  f"{dist_id}_grid_{y}.png")
         grid_image.save(save_path)
