9ddfc34ffb13fd226aca3bb70b203e6db96b65c5
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-30 13:28:35 +0100

    mnist

--- main.py
+++ main.py
@@ -72,7 +72,9 @@ def run_pipnet(args=None):
     
     # Create a convolutional network based on arguments and add 1x1 conv layer
     feature_net, add_on_layers, pool_layer, classification_layer, num_prototypes = get_network(len(classes), args)
-   
+
+    writer = SummaryWriter(log_dir=args.log_dir)
+
     # Create a PIP-Net
     net = PIPNet(num_classes=len(classes),
                     num_prototypes=num_prototypes,
@@ -344,28 +346,28 @@ def run_pipnet(args=None):
             print("Class", c, "(", list(testloader.dataset.class_to_idx.keys())[list(testloader.dataset.class_to_idx.values()).index(c)],"):","has", len(relevant_ps),"relevant prototypes: ", relevant_ps, flush=True)
 
     # Evaluate prototype purity        
-    if args.dataset == 'CUB-200-2011':
-        projectset_img0_path = projectloader.dataset.samples[0][0]
-        project_path = os.path.split(os.path.split(projectset_img0_path)[0])[0].split("dataset")[0]
-        parts_loc_path = os.path.join(project_path, "parts/part_locs.txt")
-        parts_name_path = os.path.join(project_path, "parts/parts.txt")
-        imgs_id_path = os.path.join(project_path, "images.txt")
-        cubthreshold = 0.5 
-
-        net.eval()
-        print("\n\nEvaluating cub prototypes for training set", flush=True)        
-        csvfile_topk = get_topk_cub(net, projectloader, 10, 'train_'+str(epoch), device, args)
-        eval_prototypes_cub_parts_csv(csvfile_topk, parts_loc_path, parts_name_path, imgs_id_path, 'train_topk_'+str(epoch), args, log)
-        
-        csvfile_all = get_proto_patches_cub(net, projectloader, 'train_all_'+str(epoch), device, args, threshold=cubthreshold)
-        eval_prototypes_cub_parts_csv(csvfile_all, parts_loc_path, parts_name_path, imgs_id_path, 'train_all_thres'+str(cubthreshold)+'_'+str(epoch), args, log)
-        
-        print("\n\nEvaluating cub prototypes for test set", flush=True)
-        csvfile_topk = get_topk_cub(net, test_projectloader, 10, 'test_'+str(epoch), device, args)
-        eval_prototypes_cub_parts_csv(csvfile_topk, parts_loc_path, parts_name_path, imgs_id_path, 'test_topk_'+str(epoch), args, log)
-        cubthreshold = 0.5
-        csvfile_all = get_proto_patches_cub(net, test_projectloader, 'test_'+str(epoch), device, args, threshold=cubthreshold)
-        eval_prototypes_cub_parts_csv(csvfile_all, parts_loc_path, parts_name_path, imgs_id_path, 'test_all_thres'+str(cubthreshold)+'_'+str(epoch), args, log)
+    # if args.dataset == 'CUB-200-2011':
+    #     projectset_img0_path = projectloader.dataset.samples[0][0]
+    #     project_path = os.path.split(os.path.split(projectset_img0_path)[0])[0].split("dataset")[0]
+    #     parts_loc_path = os.path.join(project_path, "parts/part_locs.txt")
+    #     parts_name_path = os.path.join(project_path, "parts/parts.txt")
+    #     imgs_id_path = os.path.join(project_path, "images.txt")
+    #     cubthreshold = 0.5
+    #
+    #     net.eval()
+    #     print("\n\nEvaluating cub prototypes for training set", flush=True)
+    #     csvfile_topk = get_topk_cub(net, projectloader, 10, 'train_'+str(epoch), device, args)
+    #     eval_prototypes_cub_parts_csv(csvfile_topk, parts_loc_path, parts_name_path, imgs_id_path, 'train_topk_'+str(epoch), args, log)
+    #
+    #     csvfile_all = get_proto_patches_cub(net, projectloader, 'train_all_'+str(epoch), device, args, threshold=cubthreshold)
+    #     eval_prototypes_cub_parts_csv(csvfile_all, parts_loc_path, parts_name_path, imgs_id_path, 'train_all_thres'+str(cubthreshold)+'_'+str(epoch), args, log)
+    #
+    #     print("\n\nEvaluating cub prototypes for test set", flush=True)
+    #     csvfile_topk = get_topk_cub(net, test_projectloader, 10, 'test_'+str(epoch), device, args)
+    #     eval_prototypes_cub_parts_csv(csvfile_topk, parts_loc_path, parts_name_path, imgs_id_path, 'test_topk_'+str(epoch), args, log)
+    #     cubthreshold = 0.5
+    #     csvfile_all = get_proto_patches_cub(net, test_projectloader, 'test_'+str(epoch), device, args, threshold=cubthreshold)
+    #     eval_prototypes_cub_parts_csv(csvfile_all, parts_loc_path, parts_name_path, imgs_id_path, 'test_all_thres'+str(cubthreshold)+'_'+str(epoch), args, log)
         
     # visualize predictions 
     visualize(net, projectloader, len(classes), device, 'visualised_prototypes', args)
