c697ea04d1c6035f67e333dbbd67d8da17f61feb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-01 13:11:23 +0100

    added plots

--- util/data.py
+++ util/data.py
@@ -442,7 +442,7 @@ def shuffle_image_patches(img, patch_size=32):
     #img = F.resize(img, (224, 224))
 
     # Convert PIL image to tensor
-    img_tensor = F.to_tensor(img)
+    #img_tensor = F.to_tensor(img)
 
     # Get image dimensions
     c, h, w = img_tensor.shape
@@ -474,8 +474,8 @@ def shuffle_image_patches(img, patch_size=32):
 
 
 def create_boolean_mask(mask_img):
-    mask_img = shuffle_image_patches(mask_img)
     mask_img = F.rgb_to_grayscale(mask_img)
+    mask_img = shuffle_image_patches(mask_img)
 
     m_shape = mask_img.shape
     if len(m_shape) == 3:
