b8699c7f0763bf0984f2412153331ccef37b5dd1
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-07 13:28:20 +0100

    added plots

--- util/vis_pipnet.py
+++ util/vis_pipnet.py
@@ -102,6 +102,7 @@ def visualize_topk(net, projectloader, num_classes, device, foldername, args: ar
                 c_weight = torch.max(classification_weights[:,p])
                 #print('c_weight ', c_weight)
                 if c_weight > 1e-3:#ignore prototypes that are not relevant to any class
+                    print(p, c_weight)
                     #p_x = visualize_dist(pfs[p].view(-1).cpu(), ys)
                     if i not in img_dist.keys():
                         img_dist[i] = []
