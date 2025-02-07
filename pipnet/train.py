e6c110bb9647d356af729a9a3f13fcf0949d772f
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-07 16:16:17 +0100

    added plots

--- pipnet/train.py
+++ pipnet/train.py
@@ -74,6 +74,8 @@ def train_pipnet(net, train_loader, optimizer_net, optimizer_classifier, schedul
         # Compute the gradient
         loss.backward()
 
+        print(pretrain, epoch)
+
         if pretrain and epoch == 1:
             for c in net.module._num_classes:
                 print(ys == c)
