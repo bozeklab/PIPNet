8d9b2033d560b8bda93a2d03d877d983f27e46ea
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-07 17:13:48 +0100

    added plots

--- pipnet/train.py
+++ pipnet/train.py
@@ -77,9 +77,10 @@ def train_pipnet(net, train_loader, optimizer_net, optimizer_classifier, schedul
                 #print('!!! ', proto_features[ys == c, ...].shape, class_mpc[c, ...].shape)
                 class_mpc[c, ...] += proto_features[ys == c, ...].flatten(2).sum(0)
     #class_mpc = torch.mean(class_mpc, dim=0)
-    dist_ps = []
+
     print('!!!! ', class_mpc.shape)
     for c in range(net.module._num_classes):
+        dist_ps = []
         for p in range(32):
             dist_ps.append(visualize_dist(class_mpc[c, p, :].detach().cpu()))
         grid_image = build_image_grid(dist_ps)
