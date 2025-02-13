15f166950793f4da20e08f67b0d2772e06ec453a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-13 17:39:30 +0100

    added mnist test

--- mnist_test.py
+++ mnist_test.py
@@ -46,6 +46,8 @@ with torch.no_grad():
     for images, labels in test_loader:
         images, labels = images.to(device), labels.to(device)
         outputs = model(images)
+        print('!!! ', outputs.shape)
+
         _, predicted = torch.max(outputs, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
