83f869c097f4de7e26f8da3a482cc2690d5344ea
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-13 17:37:26 +0100

    added mnist test

--- mnist_test.py
+++ mnist_test.py
@@ -39,6 +39,20 @@ model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+print('First evaluation: ')
+model.eval()
+correct, total = 0, 0
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+accuracy = 100 * correct / total
+print(f"Test Accuracy: {accuracy:.2f}%")
+
 
 num_epochs = 3
 print("Training started...")
