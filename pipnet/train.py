43a2cd310f3efc1ed65cc79801f15d808f079db1
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-13 10:30:04 +0100

    added plots

--- pipnet/train.py
+++ pipnet/train.py
@@ -76,7 +76,7 @@ def train_pipnet(net, train_loader, optimizer_net, optimizer_classifier, schedul
             proto_features, pooled, out = net(xs1)
 
             for c in range(net.module._num_classes):
-                #print('!!! ', proto_features[ys == c, ...].shape, class_mpc[c, ...].shape)
+                print('!!! c', proto_features[ys == c, ...].shape, class_mpc[c, ...].shape)
                 class_mpc[c, ...] += proto_features[ys == c, ...].flatten(2).sum(0)
                 class_counts[c] += (ys == c).sum()
         class_mpc /= class_counts.view(-1, 1, 1)
