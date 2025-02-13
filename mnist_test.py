a38a6bf9d4c97476263a3c990e9dfcce12d883a7
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-13 18:43:24 +0100

    added mnist test

--- mnist_test.py
+++ mnist_test.py
@@ -80,7 +80,7 @@ model = TileClassifier(model).to(device)
 
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
 # Training function
