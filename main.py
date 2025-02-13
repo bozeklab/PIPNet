8424ff3e50fc983679c02a7288cebc92f86c2ceb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-13 20:22:03 +0100

    added mnist test

--- main.py
+++ main.py
@@ -74,10 +74,10 @@ def run_pipnet(args=None):
 
     feature_net, add_on_layers, pool_layer, classification_layer, num_prototypes = get_network(len(classes), args)
     model_path = "convnext_mnist.pth"
-    print("Loading MNIST ConvNeXT weights into feature_net...")
-    missing_keys, unexpected_keys = feature_net.load_state_dict(torch.load(model_path, map_location=device), strict=False)
-    print('Missing keys: ', missing_keys)
-    print('Unexected keys: ', unexpected_keys)
+    #print("Loading MNIST ConvNeXT weights into feature_net...")
+    #missing_keys, unexpected_keys = feature_net.load_state_dict(torch.load(model_path, map_location=device), strict=False)
+    #print('Missing keys: ', missing_keys)
+    #print('Unexected keys: ', unexpected_keys)
 
 
     writer = SummaryWriter(log_dir=args.log_dir)
--- pipnet/train.py
+++ pipnet/train.py
@@ -74,6 +74,9 @@ def train_pipnet(net, train_loader, optimizer_net, optimizer_classifier, schedul
             xs1, xs2, ys = xs1.to(device), xs2.to(device), ys.to(device)
 
             proto_features, pooled, out = net(xs1)
+            argmax_indices = torch.argmax(proto_features, dim=1, keepdim=True)
+            mask = torch.zeros_like(proto_features).scatter_(1, argmax_indices, 1)
+            proto_features = proto_features * mask
 
             for c in range(net.module._num_classes):
                 print('!!! c == ', c,  proto_features[ys == c, ...].shape, class_mpc[c, ...].shape, ys.shape)
