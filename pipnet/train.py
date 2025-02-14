a3b1416fb2d2b32c7e8c84833585ed7aa0a43d63
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-02-14 11:37:53 +0100

    remove classification

--- pipnet/train.py
+++ pipnet/train.py
@@ -157,6 +157,8 @@ def train_pipnet(net, train_loader, optimizer_net, scheduler_net, criterion, epo
                     ncols=0)
 
     with torch.no_grad():
+        class_mpc.zero_()
+        class_counts.zero_()
         for _, (xs1, xs2, ys) in class_mpc_iter:
             xs1, xs2, ys = xs1.to(device), xs2.to(device), ys.to(device)
 
@@ -169,12 +171,12 @@ def train_pipnet(net, train_loader, optimizer_net, scheduler_net, criterion, epo
                 # print('!!! c == ', c,  proto_features[ys == c, ...].shape, class_mpc[c, ...].shape, ys.shape)
                 # print(F.mse_loss(proto_features[ys == 0, ...].flatten(2).sum(0), proto_features[ys == 1, ...].flatten(2).sum(0)))
                 pf = proto_features[ys == c, ...].flatten(2).sum(0)
-                pf_s, _ = torch.sort(pf, dim=1)
-                class_mpc[c, ...] += pf_s
+                class_mpc[c, ...] += pf
                 #mse_loss = F.mse_loss(class_mpc[0], class_mpc[1])
                 #
                 class_counts[c] += (ys == c).sum()
         class_mpc /= class_counts.view(-1, 1, 1)
+        class_mpc, _ = torch.sort(class_mpc, dim=2)
         dist_ps = []
         for p in range(32):
             dist_ps.append(visualize_two_dists(class_mpc[0, p, :].detach().cpu(), class_mpc[1, p, :].detach().cpu()))
