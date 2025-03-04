c8ef2fc3838c576e7c905e497ced5382973e216a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-03-04 14:07:43 +0100

    green cubes

--- scenes/green_cubes.py
+++ scenes/green_cubes.py
@@ -5,13 +5,15 @@ import shutil
 # Load JSON file
 json_path = "/data/pwojcik/whole_scene_data.json"
 image_folder = "/data/pwojcik/images/"  # Update with correct path
-destination_folder = "/data/pwojcik/shapes4/train/cubes_4_10/"
+destination_folder_1_3 = "/data/pwojcik/shapes4/train/cubes_1_3/"
+destination_folder_4_10 = "/data/pwojcik/shapes4/train/cubes_4_10/"
 
 with open(json_path, "r") as file:
     data = json.load(file)
 
 # Store filenames that meet the condition
-matching_filenames = []
+matching_filenames_1_3 = []
+matching_filenames_4_10 = []
 
 
 for filename, details in data.items():
@@ -19,14 +21,23 @@ for filename, details in data.items():
         1 for obj in details["objects"] if obj["shape"] == "cube" and obj["color"] == "green"
     )
 
+    if 1 <= green_cube_count <= 3:
+        matching_filenames_1_3.append(filename)
     if 4 <= green_cube_count <= 10:
-        matching_filenames.append(filename)
+        matching_filenames_4_10.append(filename)
 
 # Copy matching files
-for fname in matching_filenames:
+for fname in matching_filenames_1_3:
     image_file = os.path.join(image_folder, f"{fname}.png")  # Assuming images are PNGs
     if os.path.exists(image_file):
-        shutil.copy(image_file, destination_folder)
-        print(f"Copied {image_file} to {destination_folder}")
+        shutil.copy(image_file, destination_folder_1_3)
+        print(f"Copied {image_file} to {destination_folder_1_3}")
 
-print(f"Copied {len(matching_filenames)} images to {destination_folder}")
+for fname in matching_filenames_4_10:
+    image_file = os.path.join(image_folder, f"{fname}.png")  # Assuming images are PNGs
+    if os.path.exists(image_file):
+        shutil.copy(image_file, destination_folder_4_10)
+        print(f"Copied {image_file} to {destination_folder_4_10}")
+
+print(f"Copied {len(matching_filenames_1_3)} images to {destination_folder_1_3}")
+print(f"Copied {len(matching_filenames_4_10)} images to {destination_folder_4_10}")
