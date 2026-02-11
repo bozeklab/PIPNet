acc743e4622bb722881f41d063d9713c656144be
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-02-11 10:48:05 +0100

    clev4 dataset

--- util/data.py
+++ util/data.py
@@ -475,8 +475,7 @@ def shuffle_image_patches(img, patch_size=32):
 
 def create_boolean_mask(mask_img):
     mask_img = F.rgb_to_grayscale(mask_img)
-
-    mask_img = shuffle_image_patches(mask_img)
+    #mask_img = shuffle_image_patches(mask_img)
 
     m_shape = mask_img.shape
     if len(m_shape) == 3:
