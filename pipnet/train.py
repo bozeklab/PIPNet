b45e2b3fa1e867597df58875f903d9fedc3470f6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-02-25 00:57:26 +0100

    masks

--- pipnet/train.py
+++ pipnet/train.py
@@ -214,13 +214,13 @@ def train_pipnet(net, train_loader, optimizer_net, optimizer_classifier, schedul
         proto_features_bal = sinkhorn_balance_probs_in_mask(
             proto_features, visible_mask_big,
             n_iters=5,
-            momentum=0.2,
+            momentum=0.4,
         )
 
         proto_features_ds_bal = sinkhorn_balance_probs_in_mask(
             proto_features_ds, visible_mask_ds,
             n_iters=5,
-            momentum=0.2,
+            momentum=0.4,
         )
 
         # ---- recompute pooled/out so loss sees the balanced maps ----
